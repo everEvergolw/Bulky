31acc18f18d7758cb64eba3b73af96c17e9ee981
Everglow <1092547508@qq.com>
2024-03-14 20:22:43 +0000

    Update Identity

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -19,8 +19,8 @@ namespace BulkyBook.DataAccess.Repository
 		public Repository(ApplicationDbContext db)
 		{
 			_db = db;
-			this.dbSet = _db.Set<T>(); 
-			
+			this.dbSet = _db.Set<T>();
+			_db.Products.Include(u => u.Category).Include(u => u.Category.Id);
 		}
 
 		public void Add(T entity)
@@ -29,19 +29,40 @@ namespace BulkyBook.DataAccess.Repository
 
 		}
 
-		public T Get(Expression<Func<T, bool>> filter)
+		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
 		{
 			IQueryable<T> query = dbSet;
 			query = query.Where(filter);
+			if (!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach (var includeProp in includeProperties.
+					Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+
+
+			}
 
 			return query.FirstOrDefault();
 
 		}
 
-		public IEnumerable<T> GetAll()
+		//Category,CategoryId CoverType
+		public IEnumerable<T> GetAll(string? includeProperties = null)
 		{
-			IQueryable<T> query = dbSet;
-			return query.ToList();
+			IQueryable<T> query = dbSet; 
+			if(!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach(var includeProp in includeProperties.
+					Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+
+
+			}
+			return query.ToList(); 
 
 
 		}
--- Bulky.Models/ViewModels/ProductVM.cs
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBook.Models.ViewModels
@@ -10,6 +12,7 @@ namespace BulkyBook.Models.ViewModels
 	public class ProductVM
 	{
 		public Product Product { get; set; }
+		[ValidateNever] 
 		public IEnumerable<SelectListItem> CategoryList { get; set; }
 
 
